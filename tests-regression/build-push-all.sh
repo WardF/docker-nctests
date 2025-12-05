13ed5accb76f4854532ea9ef822d46bfc9d10f45
Ward Fisher <wfisher@ucar.edu>
2025-12-05 16:38:20 -0700

    Removed a couple obsolete files, added new quick-build.sh file.

---
+++ tests-regression/build-push-all.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# 
+# Utility script to build for 3 architectures.
+
+set -e
+
+echo -e ""
+echo -e "Building docker.unidata.ucar.edu/nctests for the following architectures:"
+echo -e "\to linux/arm64"
+echo -e "\to linux/amd64"
+echo -e "\to linux/s390x"
+echo ""
+echo "!!! This will take a while! Up to two hours or more! !!!"
+echo ""
+echo -e "[Press Return to Continue]"
+echo ""
+read
+
+time docker build -t docker.unidata.ucar.edu/nctests -f Dockerfile.nctests --platform linux/arm64,linux/amd64,linux/s390x . --push
+
